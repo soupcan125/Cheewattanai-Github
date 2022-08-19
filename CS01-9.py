d871c33afd6088ca34b6e87167fff450f9a1f96e
ชีวัธนัย ทิพย์กาญจนกุล <105931752+soupcan125@users.noreply.github.com>
2022-08-19 16:20:43 +0700

    1

---
+++ CS01-9.py
@@ -0,0 +1,6 @@
+N1 = int(input("Input First Number: "))
+N2 = int(input("Input Second Number: "))
+N3 = int(input("Input Third Number: "))
+if N1 > N2 and N1 < N3:
+    print("condition is True");
+
