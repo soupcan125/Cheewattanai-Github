c4327e40686cbea6bb0484988ade3e0aca8a7fbc
ชีวัธนัย ทิพย์กาญจนกุล <105931752+soupcan125@users.noreply.github.com>
2022-09-02 16:01:04 +0700

    2

---
+++ CS01-14.py
@@ -0,0 +1,3 @@
+a = ["Cheewattanai","Tipkanjanakul","38496"]
+e = a[0,1,2]
+print(e)
---
+++ CS01-15.py
@@ -0,0 +1,6 @@
+a = []
+for i in range(int(input("input total number : "))):
+    n = int(input("input number : "))
+    a.append(n)
+print(min(a))
+print(max(a))
